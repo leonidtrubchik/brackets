d21b9a682d3c496a199742e2b49786e649d00fd4
Leonid Trubchik <leon.d.e@mail.ru>
2021-03-16 02:58:00 +0300

    init: complete the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr  = [];
+
+  bracketsConfig.forEach( value => arr.push(value.join('')) );
+
+  for (let i = 0; i < arr.length; i++) {
+
+    if (str.includes(arr[i]) ){
+      str = str.replace(arr[i], '');
+      i = -1;
+    }
+  }
+  return str.length === 0 ? true : false;
 }
